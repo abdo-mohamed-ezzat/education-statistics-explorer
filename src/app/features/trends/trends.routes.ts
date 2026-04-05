9cfcc267eb4ede3b8267be69e693c67533cca1e8
abdo-mohamed-ezzat <norelganna@gmail.com>
2026-04-05 23:58:32 +0200

    feat: add master page component and route configuratoin

--- src/app/features/trends/trends.routes.ts
+++ src/app/features/trends/trends.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from "@angular/router";
 
 export const trendsRoutes: Routes = [
-    
+  {
+    path: '',
+    loadComponent: () => import('./pages/trends-page.component').then((m) => m.TrendsPageComponent),
+  },
 ]

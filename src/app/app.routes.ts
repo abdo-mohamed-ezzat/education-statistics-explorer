e9c2eea5adadaccb351cb65ef3919e492e5145d2
abdo-mohamed-ezzat <norelganna@gmail.com>
2026-04-06 13:09:00 +0200

    ref: inversion of layer coloring and improve layout styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,23 @@
 import { Routes } from '@angular/router';
 export const routes: Routes = [
-    { path: '', redirectTo: 'overview', pathMatch: 'full' },
-    { path: 'overview', loadChildren: () => import('./features/overview/overview.routes').then(m => m.overviewRoutes) },
-    { path: 'trends', loadChildren: () => import('./features/trends/trends.routes').then(m => m.trendsRoutes) },
-    { path: 'regional-analysis', loadChildren: () => import('./features/regional-analysis/regional-analysis.routes').then(m => m.regionalAnalysisRoutes) },
-    { path: '**', redirectTo: 'overview' }
+  { path: '', redirectTo: 'overview', pathMatch: 'full' },
+  {
+    path: 'overview',
+    loadChildren: () => import('./features/overview/overview.routes').then((m) => m.overviewRoutes),
+    data: { titleKey: 'overview.page-title' },
+  },
+  {
+    path: 'trends',
+    loadChildren: () => import('./features/trends/trends.routes').then((m) => m.trendsRoutes),
+    data: { titleKey: 'trends.page-title' },
+  },
+  {
+    path: 'regional-analysis',
+    loadChildren: () =>
+      import('./features/regional-analysis/regional-analysis.routes').then(
+        (m) => m.regionalAnalysisRoutes,
+      ),
+    data: { titleKey: 'regional-analysis.page-title' },
+  },
+  { path: '**', redirectTo: 'overview' },
 ];
